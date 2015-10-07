90c487865bd80b48b2625cab7407fb32e2098bc8
Nicolas Chambrier <naholyr@gmail.com>
2015-10-07 17:46:25 +0200

    npm run deploy

---
+++ aws-upload.conf.js
@@ -0,0 +1,12 @@
+module.exports = {
+  credentials: "aws-credentials.json",
+  bucketName: "byteclub.fr",
+  patterns:[
+    "css/*.css",
+    "js/*.js",
+    "img/*.*",
+    "img/tmp/*.*",
+    "img/logo clients/*.*",
+    "*.html"
+  ]
+}
