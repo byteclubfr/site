5fd8c0443cf41a62ff775c44cb5a407e8c6e6ad2
Delapouite <delapouite@gmail.com>
2015-10-08 10:49:07 +0200

    fix(deploy): handle relative build dir

--- aws-upload.conf.js
+++ aws-upload.conf.js
@@ -2,11 +2,11 @@ module.exports = {
   credentials: "aws-credentials.json",
   bucketName: "byteclub.fr",
   patterns:[
-    "build/css/*.css",
-    "build/js/*.js",
-    "build/img/*.*",
-    "build/img/avatars/*.*",
-    "build/img/logos-clients/*.*",
-    "build/*.html"
+    "css/*.css",
+    "js/*.js",
+    "img/*.*",
+    "img/avatars/*.*",
+    "img/logos-clients/*.*",
+    "*.html"
   ]
 }
